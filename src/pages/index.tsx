c2388da538854030f57c92d0041a9a265ec3da32
chainmal3 <475.lien@gmail.com>
2025-03-22 17:05:41 +1100

    index.tsx basic code added

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Sound Systems of Melbourne</h1>
+      <p>This page is under construction...thank you for your patience.</p>
+    </div>
+  )
+}
